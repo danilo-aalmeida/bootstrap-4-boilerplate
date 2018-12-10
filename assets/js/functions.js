d7cd8b47580e5c406b8e6a50f8f9ea03df90b1b3
Danilo Almeida <danilo.a.almeida@live.jp>
2018-12-10 12:31:07 -0200

    inclusão-alert-js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,11 @@
 $( document ).ready(function() {
+  $("#btnCadastrar").click(function () {
+    var nome = document.getElementById('nomeCadastro');
+    var email = document.getElementById('emailCadastro');
+    window.alert(nome.value + ' seja bem vinda(o) ao AcolheDoulas. \n Em breve entraremos em contato através do e-mail: ' + email.value + ".");
+  });
 
   // Get started!
 
 });
 
-function confirmEmail(){
-  alert('Obrigado por se cadastrar!');
-}
